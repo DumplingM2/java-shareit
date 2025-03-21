7e05b2ba0934b808722a59a6859a5b1d1f31e4d9
Aleksei <andreev@almaz.spb.ru>
2025-03-21 15:07:29 +0300

    Откорректировал Checkstyle

--- src/main/java/ru/practicum/shareit/item/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -5,9 +5,14 @@ import ru.practicum.shareit.item.model.Item;
 import java.util.List;
 
 public interface ItemService {
+
     Item addItem(Long ownerId, Item item);
+
     Item updateItem(Long ownerId, Long itemId, Item item);
+
     Item getItem(Long userId, Long itemId);
+
     List<Item> getItemsByOwner(Long ownerId);
+
     List<Item> searchItems(String text);
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -6,11 +6,13 @@ import lombok.Setter;
 @Setter
 @Getter
 public class Item {
+    // Геттеры и сеттеры
     private Long id;
     private String name;
     private String description;
     private Boolean available;
-    private Long ownerId; // идентификатор владельца
+    private Long ownerId;
 
-    public Item() { }
+    public Item() {
+    }
 }
