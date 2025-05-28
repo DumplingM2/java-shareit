26e6fe4649da86fc2523ab2f15e5973d5166deb2
Aleksei <andreev@almaz.spb.ru>
2025-05-28 17:04:53 +0300

    Переписал ShareIt по ТЗ Спринт 16.

--- src/main/java/ru/practicum/shareit/item/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.item;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-@SuppressWarnings("unused")
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
